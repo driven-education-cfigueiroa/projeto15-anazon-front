0701b03abfd2c61bc9c433137e0125ab9d1b338f
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-25 19:17:25 -0300

    chore: remove unused code

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,3 @@
-
 export const Rating = ({ rating, numReviews }) => {
   return (
     <div className="rating">
--- src/utils/getError.js
+++ src/utils/getError.js
@@ -1,5 +1,4 @@
 export const getError = (error) => {
-  // console.log(error);
   return error.response && error.response.data.message
     ? error.response.data.message
     : error.message;
