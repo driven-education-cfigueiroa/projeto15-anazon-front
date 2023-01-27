b65f646dfe1c82da0f953d1400dacd36945e7c5c
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-27 01:40:40 -0300

    fix: retry request on timeout

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,16 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 
 const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const timeout = process.env.REACT_APP_API_TIMEOUT || 1000;
+const timeout = process.env.REACT_APP_API_TIMEOUT || 2000;
 
 export const api = axios.create({ baseURL, timeout });
 
-axiosRetry(api, { retries: 3 });
+axiosRetry(api, {
+  retries: 3,
+  retryCondition: (error) => {
+    return (
+      axiosRetry.isNetworkOrIdempotentRequestError(error) ||
+      error.code === 'ECONNABORTED'
+    );
+  },
+});
