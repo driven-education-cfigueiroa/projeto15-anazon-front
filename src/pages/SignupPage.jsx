567d0b8d3a94a0de10afcf4e1b0e5cd7cba1a0d3
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-27 17:17:57 -0300

    feat: add Signup Page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,34 +1,86 @@
-import { Link, useLocation } from 'react-router-dom';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Container, Form, Button } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
+import { api as axios } from '../services/api';
+import { useContext, useEffect, useState } from 'react';
+import { Store } from '../contexts/Store';
+import { toast } from 'react-toastify';
+import { getError } from '../utils/getError.js';
 
 export const SignupPage = () => {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get('redirect');
   const redirect = redirectInUrl ? redirectInUrl : '/';
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error('Senhas não conferem!');
+      return;
+    }
+    try {
+      const { data } = await axios.post('/api/users/signup', {
+        name,
+        email,
+        password,
+      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    } catch (error) {
+      console.log(error);
+      toast.error(getError(error));
+    }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
   return (
     <Container className="small-container">
       <Helmet>
         <title>Registro na Anazon</title>
       </Helmet>
       <h1 className="my-3">Criar conta</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control required />
+          <Form.Label>Nome Completo</Form.Label>
+          <Form.Control required onChange={(e) => setName(e.target.value)} />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required />
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required />
-          <Form.Group className="mb-3" controlId="confirmPassword">
-            <Form.Label>Confirm Password</Form.Label>
-            <Form.Control type="password" required />
-          </Form.Group>
+          <Form.Label>Senha</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="confirmPassword">
+          <Form.Label>Confirmar Senha</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Registrar</Button>
